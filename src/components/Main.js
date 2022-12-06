0b3e04f2003b5ceef5581b3f8ee784992bc46ffa
bubinq <emilbumov@gmail.com>
2022-12-06 13:36:42 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,39 @@
+import { axiosInstance } from "../utils";
+import { useContext, useEffect } from "react";
+import { NavigationContext } from "../contexts/navigationContext";
+import { UsersContext } from "../contexts/usersContext";
+import { VideoContext } from "../contexts/videosContext";
+import { VideoCard } from "./VideoCard";
+
+export const Main = () => {
+  const { videos, setVideos } = useContext(VideoContext);
+  const { users, setUsers } = useContext(UsersContext);
+  const { toggleSideMenu } = useContext(NavigationContext);
+
+  useEffect(() => {
+    const getVideos = async () => {
+      const response = await axiosInstance.get("/video", {
+        withCredentials: true,
+      });
+      const listOfUsers = await axiosInstance.get("/user", {
+        withCredentials: true,
+      });
+      setUsers(listOfUsers.data);
+      setVideos(response.data);
+    };
+    getVideos();
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <main>
+      <div
+        className={toggleSideMenu ? "contentWrapper" : "contentWrapperToggle"}
+      >
+        {videos &&
+          videos.map((video) => (
+            <VideoCard key={video._id} video={video} users={users}></VideoCard>
+          ))}
+      </div>
+    </main>
+  );
+};
